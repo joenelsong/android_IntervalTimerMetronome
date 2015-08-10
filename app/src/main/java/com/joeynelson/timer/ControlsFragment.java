905eb6c780ae8c4bece359258e1dc930f3532455
joenelsong <joenelsong@gmail.com>
2015-08-09 22:10:19 -0700

    Multiple Serial Timers working!, Now I need to add Metronome

--- app/src/main/java/com/joeynelson/timer/ControlsFragment.java
+++ app/src/main/java/com/joeynelson/timer/ControlsFragment.java
@@ -11,11 +11,14 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.graphics.Color;
+import android.widget.Switch;
+
 public class ControlsFragment extends Fragment {
 
     private boolean logging = true;
     private boolean twoPaneLayout;
 
+    private Switch mSwitch;
     private Spinner mTimeSigNum;
     private Spinner mTimeSigDen;
     private EditText mMinutes;
@@ -23,6 +26,10 @@ public class ControlsFragment extends Fragment {
     private EditText mBPM;
     private LinearLayout mLayout;
 
+
+    public boolean getSwitchState() {   return mSwitch.isChecked();}
+    public int getMinutes() {   return Integer.parseInt(mMinutes.getText().toString());    }
+    public int getSeconds() {   return Integer.parseInt(mSeconds.getText().toString());    }
     public Spinner getSpinner1() { return mTimeSigNum; }
     public Spinner getSpinner2() { return mTimeSigDen; }
     public void setSpinner1(int x) { mTimeSigNum.setSelection(x); }
@@ -34,12 +41,13 @@ public class ControlsFragment extends Fragment {
         if (logging) Log.d("ControlsFragment", "Start: onCreateView()");
         View view = inflater.inflate(R.layout.fragment_controls, container, false);
 
-        // Set Spinner Default Values
-        mTimeSigNum = (Spinner) view.findViewById(R.id.rythmspinner1);
-    //Log.d("ControlsFragment", "sp =" + mTimeSigNum);
+        mSwitch = (Switch) view.findViewById(R.id.queueSwitch);
+
+
+        mTimeSigNum = (Spinner) view.findViewById(R.id.rythmspinner1); //Log.d("ControlsFragment", "sp =" + mTimeSigNum);
         mTimeSigDen = (Spinner) view.findViewById(R.id.rythmspinner2);
-        this.setSpinner1(3);
-        this.setSpinner2(3);
+        this.setSpinner1(3); // Set Spinner Default Values
+        this.setSpinner2(3); // Set Spinner Default Values
 
         mMinutes = (EditText) view.findViewById(R.id.editTextMinutes);
         mSeconds = (EditText) view.findViewById(R.id.editTextSeconds);
@@ -47,6 +55,8 @@ public class ControlsFragment extends Fragment {
 
         mLayout = (LinearLayout) view.findViewById(R.id.controlLayout);
         mLayout.setBackgroundColor(Color.LTGRAY);
+
+        mLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0,1.0f ));
 
 
 
--- app/src/main/java/com/joeynelson/timer/MainActivity.java
+++ app/src/main/java/com/joeynelson/timer/MainActivity.java
@@ -11,19 +11,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener
-{
-    private boolean logging = true;
+{   private boolean logging = true;
 
-    private int mNumberOfControls;
+    private static final String FORMAT = "%02d:%02d:%02d";
+
+    private static int mNumberOfControls;
+
+    //~// Widgets //~//
     private Button mStartButton;
+    private String mTimerState = "STOPPED";
+    private TextView mTime;
 
 
     // Timer Variables //
     private CountDownTimer mTimer;
-    private int mActiveTimerIndex;
+    protected int mActiveTimerIndex=0;
     private ControlsFragment[] mControlsFragments;
 
     @Override
@@ -35,6 +43,8 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         // Initialize Widgets //
         mStartButton = (Button) findViewById(R.id.start_button);
         mStartButton.setOnClickListener(this);
+
+        mTime = (TextView) findViewById(R.id.clock_display);
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
@@ -72,15 +82,41 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     }
     @Override
     public void onClick(View view)
-    {
+    { if (logging) Log.d("onClick", "Start");
         switch(view.getId())
         {
-        case R.id.start_button:
-            highlightTimer(1);
+        case R.id.start_button: Log.d("onClick", "case R.id.start_button");
+            if (mTimerState.equals("STOPPED") && mActiveTimerIndex < mNumberOfControls)   // If a timer hasn't already started then start one, If a timer has started, dont start another one!
+            { Log.d("onClick", "if (mTimerAlreadyStarted = false)");
+                mTimerState = "STARTED";
+                //highlightTimer(mActiveTimerIndex);
+                if (mControlsFragments[mActiveTimerIndex].getSwitchState()) {
+                    int hrs = 0;
+                    int mins = mControlsFragments[mActiveTimerIndex].getMinutes();
+                    int secs = mControlsFragments[mActiveTimerIndex].getSeconds();
+                    // Create CountDownTimer
+                        CreateCountDownTimer( hrs * 3600000 + mins*60000 + secs * 1000 );
+
+                }
+            }
+            else if (mTimerState.equals("STARTED"))
+            {
+                mTimerState = "PAUSED"; // If the timer had already started, it should now be paused
+            }
+            else if (mTimerState.equals("PAUSED"))
+            {
+                mTimerState = "STARTED";
+            }
+            else { // Case where mActiveTimerIndex is now higher than the number of timers, therefore we must be done.
+                mTime.setText("DONE!");
+            }
+
 
             break;
 
-        case R.id.clear_button:
+        case R.id.clear_button: if (logging) Log.d("onClick() ++ed", "case R.id.clear_button");
+            mTimer.cancel();
+            mTimerState = "STOPPED";
             break;
         }
 
@@ -90,8 +126,32 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     /*****************************************
      * * *       Helper Functions        * * *
      ****************************************/
-    public void UpdateTimerStates()
-    {
+    public void CreateCountDownTimer(int ms)
+    { if (logging) Log.d("CreateCountDownTimer", "Start");
+
+        highlightTimer(mActiveTimerIndex);
+        if (mActiveTimerIndex > 0){
+            unhighlightTimer(mActiveTimerIndex-1);
+        }
+        mTimer = new CountDownTimer(ms, 1000) { // adjust the milli seconds here
+
+
+            public void onTick(long millisUntilFinished) {
+
+                mTime.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            public void onFinish() { if (logging) Log.d("OnFinish()", "CountdownTimer Finished");
+                mTimerState = "STOPPED";
+                mActiveTimerIndex = mActiveTimerIndex + 1;
+                onClick(mStartButton);
+            }
+        }.start();
 
     }
     public void highlightTimer(int x)
@@ -102,6 +162,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     {
         mControlsFragments[x].setLayoutColor(Color.LTGRAY);
     }
+    public void setNewActiveTimerIndex(int x)   { mActiveTimerIndex=+1%mNumberOfControls; }
 
 
 
